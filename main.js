8cbd90cd3511eadec6bfc9d7a6b8acc8a333e896
Alexandre <alexandre.cesar.1995@hotmail.com>
2024-04-08 15:20:19 -0300

    segunda pagina sobre nós

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector("button").addEventListener("click", entrar);
+
+<a href='https://api.whatsapp.com/message/LB4M3DX3IJWPC1?autoload=1&app_absent=0'></a>;
+window.location.href = "https://api.whatsapp.com/message/LB4M3DX3IJWPC1?autoload=1&app_absent=0";
